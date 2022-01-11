e8c9ffc7e6b9e6681b38e42602502d3e19a20011
tsy <984337688@qq.com>
2022-01-11 21:30:05 +0800

    打包前的提交

--- blog-api/src/main/java/com/tsy/blog/config/WebConfiguration.java
+++ blog-api/src/main/java/com/tsy/blog/config/WebConfiguration.java
@@ -19,15 +19,19 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     /**
      * 自定义跨域规则，允许来自8080端口的前端项目访问
+     *
      * @param registry -
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedHeaders("http://yileng.yop");
     }
 
     /**
      * 注册拦截器
+     *
      * @param registry -
      */
     @Override
